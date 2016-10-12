fa441f04a377e307d1ece73a71c5333659a6be2d
Mattias Hammarhorn <mattias@hammarhorn.se>
2016-10-12 11:45:33 +0200

    Testing

---
+++ Labb 1 - Klasser och Objekt/Labb 1 - Klasser och Objekt/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___Klasser_och_Objekt
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        //Tom konstruktor
+        public Dog()
+        {
+        }
+
+        public Dog(string name, int age, string breed)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+        }
+    }
+}
---
+++ Labb 1 - Klasser och Objekt/Labb 1 - Klasser och Objekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___Klasser_och_Objekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
