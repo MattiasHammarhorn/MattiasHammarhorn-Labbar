f8cb65c8c3a6a22bf529d9b6bf91f5baf17b785e
Mattias Hammarhorn <mattias@hammarhorn.se>
2016-10-14 15:02:23 +0200

    1st

---
+++ Labbar/Labb 1 - Klasser och Objekt/Labb 1 - Klasser och Objekt/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___Klasser_och_Objekt
+{
+    class Dog
+    {
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        //Tom konstruktor
+        public Dog()
+        {
+        }
+        //Konstruktor för Dog
+        public Dog(string name, int age, string breed)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+        }
+
+        public string DogInformation()
+        {
+            return String.Format("Name: {0} Age: {1} Breed: {2}", Name, Age, Breed);
+        }
+    }
+}
---
+++ Labbar/Labb 1 - Klasser och Objekt/Labb 1 - Klasser och Objekt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___Klasser_och_Objekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runtime = new Runtime();
+            runtime.Start();
+        }
+    }
+}
